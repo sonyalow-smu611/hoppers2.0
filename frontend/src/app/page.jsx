04f8e5cdbb069bd5939cfc1456f47392949417b2
enyacjy <enya.chan.2025@smu.edu.sg>
2026-07-03 20:53:48 +0800

    map

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,25 +1,16 @@
-import Image from "next/image";
-import api from "../api";
-import Dashboard from "../app/Dashboard/page";
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+import Map from "@/components/map/Map";
 
-//     </div>
-//   );
-// }
-
-export default async function HomePage() {
-  const response = await api.get("http://localhost:4000/cafes");
-
-  console.log(response.data);
-
-  const cafeList = response.data.cafes;
-
+export default function MapPage() {
   return (
-    <>
-      <h1>Insert map here</h1>
-      <Dashboard />
-    </>
+    <main className="min-h-screen bg-white p-6">
+      <div className="mx-auto max-w-5xl space-y-6">
+        <input
+          placeholder="Search for cafes..."
+          className="w-full rounded-full border px-5 py-3 text-sm shadow-sm"
+        />
+
+        <Map />
+      </div>
+    </main>
   );
 }
